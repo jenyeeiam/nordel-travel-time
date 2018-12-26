2bb1098cbe4e816e1bafb6994042e2fac635dc02
Jennifer Yee <jennifer@jennifer-macbookpro.local>
2018-12-26 09:44:02 -0800

    leaving this for now

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,50 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Map from './Map';
+import moment from 'moment';
+import {Moment} from 'moment';
+
+interface ValidState {
+  time?: Moment,
+  times?: string[]
+}
 
 class App extends Component {
+  state: ValidState
+  constructor(props: any) {
+    super(props);
+    this.state = {}
+  }
+
+  handleTimeSlider(value: number) {
+    if(this.state.times) {
+      this.setState({time: moment(this.state.times[value])})
+
+    }
+  }
+
+  handleSetTime(time: string, times: string[]) {
+    this.setState({time: moment(time), times})
+  }
+
   render() {
     return (
       <div className="App">
-        <Map />
-        <div className='footer'>
-        </div>
+        <Map
+          setTimes={this.handleSetTime.bind(this)}
+          time={moment(this.state.time).format('YYYY-MM-DD HH:mm:ss')}
+        />
+        <div
+          className='clock'
+          onClick={() => {
+            let t0 = 10000;
+            setInterval(() => {
+              t0 += 5;
+              this.handleTimeSlider(t0)
+            }, 250)
+          }}
+        >{moment(this.state.time).format('HH:mm:ss') || ''}</div>
+
       </div>
     );
   }
--- src/Map.tsx
+++ src/Map.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import mapboxgl from 'mapbox-gl';
 import Papa from 'papaparse';
+import {uniq} from 'lodash';
 var csvFilePath = require("./data/travel_time_points.csv");
 type MapData = {data: any[], errors: any[], meta: any};
 
@@ -12,7 +13,7 @@ interface ValidState {
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiY29tbXVuaXR5bG9naXEiLCJhIjoiY2luNmh3eXJ6MDBjM3ZqbHpuYm8zeGh1dyJ9.AeXQZPEAeFpj5C-1XSVN5Q';
 
-class MainMap extends Component {
+class MainMap extends Component<any, any> {
   state: ValidState
   constructor(props: any) {
     super(props);
@@ -24,8 +25,8 @@ class MainMap extends Component {
     let map = new mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/communitylogiq/cjhaz2wee04bz2spq1h4ji9bw',
-      center: [-122.9974365234375, 49.08353590768416],
-      zoom: 10
+      center: [-122.913555, 49.162057],
+      zoom: 13
     });
     let newPromise = (thing: mapboxgl.Map) => new Promise((resolve, reject) => {
       thing.on("style.load", function() {
@@ -35,13 +36,25 @@ class MainMap extends Component {
           "type": "circle",
           "source": {type: "geojson", data: {type: "FeatureCollection", features: [] }},
           "paint": {
-            "circle-radius": 6,
-            "circle-color": "#B42222"
+            "circle-radius": [
+              'interpolate',
+              ['linear'],
+              ['get', 'count'],
+              1, 3,
+              10, 10
+            ],
+            "circle-color": [
+              'case',
+              ['==', ['get', 'direction'], 'WB'],
+              '#FCA107',
+              '#7F3121'
+            ]
           }
         });
         resolve(thing);
       });
     })
+
     newPromise(map).then((fulfilled) => {
       this.setState({map: fulfilled})
     })
@@ -54,29 +67,24 @@ class MainMap extends Component {
   }
 
   componentDidUpdate(prevProps: any, prevState: ValidState) {
-    const {time, data, map} = this.state;
+    const {data, map} = this.state;
 
-    if(map !== prevProps.map && map) {
-      // console.log(map)
-    }
-    if(prevProps.data !== data && data) {
-      this.updateSource(data)
-    }
-    if(prevState.time !== time && map) {
-      map.setFilter("points", ["==", "datetime", time]);
+    if(prevProps.time !== this.props.time && map) {
+      map.setFilter("points", ["==", "datetime", this.props.time]);
     }
   }
 
   updateSource(data: any[]) {
-    const { map, time } = this.state;
+    const { map } = this.state;
     let features: GeoJSON.Feature[] = [];
     data.forEach(count => {
       features.push({
         type: 'Feature',
         properties: {
           datetime: count.datetime,
-          count: count.count,
-          speed: count.speed
+          count: Number(count.count),
+          speed: Number(count.speed),
+          direction: count.direction
         },
         geometry: {
           coordinates: [
@@ -87,18 +95,14 @@ class MainMap extends Component {
         }
       })
     });
-    console.log(features[0])
     let geojson: GeoJSON.FeatureCollection = {type: "FeatureCollection", features: features};
-    console.log(time)
     map && (map.getSource('points') as mapboxgl.GeoJSONSource).setData(geojson)
   }
 
   updateData(data: MapData) {
-    let dataArray = data.data;
-    this.setState({
-      data: dataArray,
-      time: dataArray[0].datetime
-    })
+    let times = uniq(data.data.map(d => d.datetime));
+    this.updateSource(data.data);
+    this.props.setTimes(data.data[0].datetime, times)
   }
 
   render() {
